8924e84b7f79d8a3baa1d5ca8faa579b0a639b5b
AntonioSouza1 <souzaantonio4020@gmail.com>
2024-10-19 20:34:07 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -20,18 +20,26 @@ arCond = {
     'arGrande': 3,  # kg por hora
     'arIndustrial': 4  # kg por hora
 }
-prodIndustrial = {
-    'ProdAco': 1.8,  # kg de co2 por kg produzido
-    'ProdCimento': 0.9,  # kg de co2 por kg produzido
-    'ProdPapel': 0.5,  # kg de co2 por kg produzido
-    'ProdVidro': 0.6,  # kg de co2 por kg produzido
-    'ProdAluminio': 12,  # kg de co2 por kg produzido
-    'ProdAmonia': 1.8,  # kg de co2 por kg produzido
-    'ProdEtanol': 1.6,  # kg de co2 por kg produzido
-    'ProdEletricidade_Carvao': 0.9,  # kg de co2 por kg produzido
-    'ProdEletricidade_GasNat': 0.4,  # kg de co2 por kg produzido
-    'ProdEletricidade_Petróleo': 0.7  # kg de co2 por kg produzido
+arCond = {
+    'arPequeno': 0.5,  # kg por hora
+    'arMedio': 1.5,  # kg por hora
+    'arGrande': 3,  # kg por hora
+    'arIndustrial': 4  # kg por hora
 }
+prodIndustrial = {
+    'prodAco': 1.8,  # kg de co2 por kg produzido
+    'prodCimento': 0.9,  # kg de co2 por kg produzido
+    'prodPapel': 0.5,  # kg de co2 por kg produzido
+    'prodVidro': 0.6,  # kg de co2 por kg produzido
+    'prodAluminio': 12,  # kg de co2 por kg produzido
+    'prodAmonia': 1.8,  # kg de co2 por kg produzido
+    'prodEtanol': 1.6,  # kg de co2 por kg produzido
+    'prodEletricidade_Carvao': 0.9,  # kg de co2 por kg produzido
+    'prodEletricidade_GasNat': 0.4,  # kg de co2 por kg produzido
+    'prodEletricidade_Petroleo': 0.7  # kg de co2 por kg produzido
+}
+
+totalEmissao = []
 @app.route('/Empresa', methods=['POST'])
 def empresa():
     # cadastro empresa
@@ -43,41 +51,69 @@ def empresa():
     dadosEmpresa['anoInventariado'] = int(request.form['ano_inventariado'])
     dadosEmpresa['ramoEmpresa'] = int(request.form['ramo_empresa'])
 
-    #entrada dos dados via formulario
+    #entrada combustivel via formulário
     etanol = float(request.form['etanol'])
     gasolina = float(request.form['gasolina'])
     diesel = float(request.form['diesel'])
     biodiesel = float(request.form['biodiesel'])
 
+    #Entrada produção via formulário
+    prodAco = float(request.form['ProdAco'])
+    prodCimento = float(request.form['ProdCimento'])
+    prodPapel = float(request.form['ProdPapel'])
+    prodVidro = float(request.form['ProdVidro'])
+    prodAluminio = float(request.form['ProdAluminio'])
+    prodEtanol = float(request.form['ProdEtanol'])
+    prodAmonia = float(request.form['ProdAmonia'])
+    prodEtanol = float(request.form['ProdEtanol'])
+    prodEletricidade_Carvao = float(request.form['ProdEletricidade_Carvao'])
+    prodEletricidade_GasNat = float(request.form['ProdEletricidade_GasNat'])
+    prodEletricidade_Petroleo = float(request.form['ProdEletricidade_Petroleo'])
+
+    #entrada ar condicionado via terminal
+    arP = float(request.form['arP'])
+    qtarP = int(request.form['qtarP'])
+    arM = float(request.form['arM'])
+    qtarM = int(request.form['qtarM'])
+    arG = float(request.form['arG'])
+    qtarG = int(request.form['qtarG'])
+    arI = float(request.form['arI'])
+    qtarI = int(request.form['qtarI'])
+
     #calculo combustível
-    etanol = (((12 * etanol)/1000) * comb['etanol'])
-    gasolina = (((12 * gasolina)/1000) * comb['gasolina'])
-    diesel = (((12 * diesel)/1000) * comb['diesel'])
-    biodiesel = (((12 * biodiesel)/1000) * comb['biodiesel'])
-    combTotal = (etanol + gasolina + diesel + biodiesel)
+    totalEmissao.append((((12 * etanol) * comb['etanol'])/1000))
+    totalEmissao.append((((12 * gasolina) * comb['gasolina'])/1000))
+    totalEmissao.append((((12 * diesel) * comb['diesel'])/1000))
+    totalEmissao.append((((12 * biodiesel) * comb['biodiesel'])/1000))
 
     #calculo produção
-    ProdAco = float(request.form['ProdAco'])
-    ProdCimento = float(request.form['ProdCimento'])
-    ProdPapel = float(request.form['ProdPapel'])
-    ProdVidro = float(request.form['ProdVidro'])
-    ProdAluminio = float(request.form['ProdAluminio'])
-    ProdEtanol = float(request.form['ProdEtanol'])
-    ProdAmonia = float(request.form['ProdAmonia'])
-    ProdEtanol = float(request.form['ProdEtanol'])
-    ProdEletricidade_Carvao = float(request.form['ProdEletricidade_Carvao'])
-    ProdEletricidade_GasNat = float(request.form['ProdEletricidade_GasNat'])
-    ProdEletricidade_Petroleo = float(request.form['ProdEletricidade_Petroleo'])
+    totalEmissao.append(((((prodAco * 30)*12) * prodIndustrial['prodAco'])/1000))
+    totalEmissao.append(((((prodCimento * 30)*12) * prodIndustrial['prodCimento'])/1000))
+    totalEmissao.append(((((prodPapel * 30)*12) * prodIndustrial['prodPapel'])/1000))
+    totalEmissao.append(((((prodVidro * 30)*12) * prodIndustrial['prodVidro'])/1000))
+    totalEmissao.append(((((prodAluminio * 30)*12) * prodIndustrial['prodAluminio'])/1000))
+    totalEmissao.append(((((prodAmonia * 30)*12) * prodIndustrial['prodAmonia'])/1000))
+    totalEmissao.append(((((prodEtanol * 30)*12) * prodIndustrial['prodEtanol'])/1000))
+    totalEmissao.append(((((prodEletricidade_Carvao * 30)*12) * prodIndustrial['prodEletricidade_Carvao'])/1000))
+    totalEmissao.append(((((prodEletricidade_GasNat * 30)*12) * prodIndustrial['prodEletricidade_GasNat'])/1000))
+    totalEmissao.append(((((prodEletricidade_Petroleo * 30)*12) * prodIndustrial['prodEletricidade_Petroleo'])/1000))
+
+    #calculo Ar condicionado
+    totalEmissao.append((((((qtarP * arP) * 30) * 12) * arCond['arPequeno']) / 1000)) #Resultado ar pequeno em toneladas
+    totalEmissao.append((((((qtarM * arM) * 30) * 12) * arCond['arMedio']) / 1000)) # Resultado ar pequeno em toneladas
+    totalEmissao.append((((((qtarG * arG) * 30) * 12) * arCond['arGrande']) / 1000)) # Resultado ar pequeno em toneladas
+    totalEmissao.append((((((qtarI * arI) * 30) * 12) * arCond['arIndustrial']) / 1000)) # Resultado ar pequeno em toneladas
 
     #retorno
-    mensagem = dadosEmpresa
-    mensagem2 = (f'Emissão total combustível: {combTotal:.2f}Toneladas co2')
+    mensagem2 = (f'{sum(totalEmissao):.2f}')
     return render_template('company_results.html',
                            nome_empresa=dadosEmpresa['nome'],
                            endereco_empresa=dadosEmpresa['endereco'],
                            nomeResponsavel_empresa=dadosEmpresa['nomeResponsavel'],
                            telefoneResponsavel_empresa=dadosEmpresa['telefoneResponsavel'],
-                           ano_inventariado=dadosEmpresa['anoInventariado'],ramo_empresa=dadosEmpresa['ramoEmpresa'],)
+                           ano_inventariado=dadosEmpresa['anoInventariado'],
+                           ramo_empresa=dadosEmpresa['ramoEmpresa'],
+                           mensagem2=mensagem2)
 
 
 @app.route('/submit2', methods=['POST'])
--- static/script.js
+++ static/script.js
@@ -1,10 +1,30 @@
+//função de relógio
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').textContent = timeString;
+}
+
+// Atualiza o relógio a cada segundo
+setInterval(updateClock, 1000);
+// Chama a função uma vez para não ficar esperando 1 segundo para aparecer
+updateClock();
+
+//apresenta o formulário de acorno com o que for selecionado
 document.addEventListener('DOMContentLoaded', function() {
     const selectElement = document.getElementById('finalidade');
-    
+    const form1 = document.getElementById('form1');
+    const form2 = document.getElementById('form2');
+    const ramo = document.getElementById('ramo_empresa');
+    const producao = document.getElementById('producao');
+    const agro = document.getElementById('agro')
+
+//apresenta o formulário de acorno com a finalidade selecionado
     selectElement.addEventListener('change', function() {
-        const form1 = document.getElementById('form1');
-        const form2 = document.getElementById('form2');
-        
         form1.classList.remove('mostrar');
         form2.classList.remove('mostrar');
         
@@ -19,4 +39,99 @@ document.addEventListener('DOMContentLoaded', function() {
             form2.style.display = 'none';
         }
     });
+//apresenta o formulário de acorno com o ramo selecionado
+    ramo.addEventListener('change', function() {
+
+        agro.classList.remove('mostrar');
+        producao.classList.remove('mostrar');
+
+        agro.style.display = 'none';
+        producao.style.display = 'none';
+
+        if (this.value === '1') {
+            agro.style.display = 'block';
+            agro.classList.add('mostrar');
+        } else if (this.value === '2') {
+            producao.style.display = 'block';
+            producao.classList.add('mostrar');
+        } else {
+            producao.style.display = 'none';
+            agro.style.display = 'none';
+        }
+    });
+})
+
+//Função oculta campo gasolina, aparece quando selecionado
+function toggleGasolina() {
+    const checkGasolina = document.getElementById('checkGasolina');
+    const gasolina = document.getElementById('gasolina');
+    const txGasolina = document.getElementById('txGasolina')
+
+    gasolina.classList.remove('mostrar');
+    txGasolina.classList.remove('mostrar');
+
+    if (checkGasolina.checked) {
+        gasolina.style.display = 'block';
+        txGasolina.style.display = 'block';
+    } else {
+        gasolina.style.display = 'none';
+        txGasolina.style.display = 'none';
+    }
+}
+//Função oculta campo etanol, aparece quando selecionado
+function toggleEtanol() {
+    const checkEtanol = document.getElementById('checkEtanol');
+    const etanol = document.getElementById('etanol');
+    const txEtanol = document.getElementById('txEtanol')
+
+    etanol.classList.remove('mostrar');
+    txEtanol.classList.remove('mostrar');
+
+    if (checkEtanol.checked) {
+        etanol.style.display = 'block';
+        txEtanol.style.display = 'block';
+    } else {
+        etanol.style.display = 'none';
+        txEtanol.style.display = 'none';
+    }
+}
+//Função oculta campo diesel, aparece quando selecionado
+function toggleDiesel() {
+    const checkEtanol = document.getElementById('checkDiesel');
+    const diesel = document.getElementById('diesel');
+    const txDiesel = document.getElementById('txDiesel')
+
+    diesel.classList.remove('mostrar');
+    txDiesel.classList.remove('mostrar');
+
+    if (checkDiesel.checked) {
+        diesel.style.display = 'block';
+        txDiesel.style.display = 'block';
+    } else {
+        diesel.style.display = 'none';
+        txDiesel.style.display = 'none';
+    }
+}
+//Função oculta campo biodiesel, aparece quando selecionado
+function toggleBiodiesel() {
+    const checkBiodiesel = document.getElementById('checkBiodiesel');
+    const biodiesel = document.getElementById('biodiesel');
+    const txBiodiesel = document.getElementById('txBiodiesel')
+
+    biodiesel.classList.remove('mostrar');
+    txBiodiesel.classList.remove('mostrar');
+
+    if (checkBiodiesel.checked) {
+        biodiesel.style.display = 'block';
+        txBiodiesel.style.display = 'block';
+    } else {
+        biodiesel.style.display = 'none';
+        txBiodiesel.style.display = 'none';
+    }
+}
+document.addEventListener('DOMContentLoaded', function() {
+    toggleGasolina();
+    toggleEtanol();
+    toggleDiesel();
+    toggleBiodiesel();
 });
